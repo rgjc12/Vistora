95501edc5b75263a9a11435ed184b51bfae547d6
Francisco Perez <fperezfrancisco4@gmail.com>
2025-04-23 22:31:58 -0700

    editted button for swiper testimonial (fjp)

--- src/components/ui/SwiperButtons.jsx
+++ src/components/ui/SwiperButtons.jsx
@@ -6,16 +6,16 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 function SwiperButtons() {
   const swiper = useSwiper();
   return (
-    <div className="z-20  w-full m-auto top-auto bottom-4 inset-10 flex items-center justify-center gap-24 p-4">
+    <div className="z-20 w-full m-auto top-auto bottom-4 inset-10 flex items-center justify-center gap-24 p-6">
       <button
         onClick={() => swiper.slidePrev()}
-        className="w-[36px] h-[36px] rounded-full bg-white flex items-center justify-center shadow-md hover:shadow-xl"
+        className="w-[36px] h-[36px] rounded-full bg-white flex items-center justify-center shadow-md hover:shadow-lg"
       >
         <ChevronLeft size={24} />
       </button>
       <button
         onClick={() => swiper.slideNext()}
-        className="w-[36px] h-[36px] rounded-full bg-white flex items-center justify-center shadow-md hover:shadow-xl"
+        className="w-[36px] h-[36px] rounded-full bg-white flex items-center justify-center shadow-md hover:shadow-lg"
       >
         <ChevronRight size={24} />
       </button>
